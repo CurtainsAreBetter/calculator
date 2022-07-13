e36f5e0272ae5ddc7ed456b663b4b9cdbad002a6
Tom <81436713+CurtainsAreBetter@users.noreply.github.com>
2022-07-13 17:29:21 -0400

    Add basic button listeners to main.js
    complete numberButtons
    complete clearScreen + button to do so

--- main.js
+++ main.js
@@ -52,3 +52,73 @@ function operate (operator, a, b){
             return 'ERROR';
     }
 }
+
+/*===========================
+        GLOBAL VARIABLES
+============================*/
+
+const screen = document.querySelector('.screen');
+let currentOperator; // see operate function for acceptable values
+let lastNumberEntry;
+let clearScreenOnNumberPress = true; 
+
+/*======================================
+        Event Handler declarations 
+========================================*/
+// Number buttons
+document.querySelectorAll('.number')
+        .forEach((num) => {
+            num.addEventListener('click', numberButtonAction);
+        });
+
+// clear button
+document.querySelector('#clear')
+        .addEventListener('click', clearButtonAction);
+
+// equals button
+document.querySelector('#equals')
+        .addEventListener('click', equalsButtonAction);
+
+// Operator buttons
+document.querySelectorAll('.operator')
+        .forEach((op) => {
+            op.addEventListener('click', operatorButtonAction);
+        });
+
+
+/*=================================
+            Utility Funcitons
+===================================*/
+function clearScreen() {
+    screen.textContent = '';
+}
+
+/* ==================================
+             Button Actions
+=====================================*/
+
+function numberButtonAction(e) {
+    if(clearScreenOnNumberPress) {
+        clearScreen();
+        clearScreenOnNumberPress = false;
+    }
+    console.log(e.target.innerText);
+    screen.textContent += e.target.innerText;
+}
+
+function clearButtonAction() {
+    clearScreen();
+    // empty global variables
+    currentOperator = ''// see operate function for acceptable values
+    lastNumberEntry = ''
+    clearScreenOnNumberPress = true;
+}
+
+function equalsButtonAction() {
+    console.log('=');
+}
+
+function operatorButtonAction(e) {
+    const operator = e.target.innerText;
+    console.log(operator);
+}
